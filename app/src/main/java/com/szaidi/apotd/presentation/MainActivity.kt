b09a1162ac32099cea0d886686a7c1c86e4cd97c
Sajjad Zaidi <szaidi@tribalscale.com>
2018-08-04 19:24:00 -0400

    Add fragments instead of replacing them.

--- app/src/main/java/com/szaidi/apotd/presentation/MainActivity.kt
+++ app/src/main/java/com/szaidi/apotd/presentation/MainActivity.kt
@@ -10,46 +10,18 @@ import com.szaidi.apotd.presentation.picturefullscreen.FullScreenImageFragment
 import kotlinx.android.synthetic.main.progress_view.*
 
 class MainActivity : AppCompatActivity() {
-
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
 		supportActionBar?.title = getString(R.string.picture_of_the_day)
 
-		addOnBackStackListener()
 		startPictureDetailFragment()
 	}
 
 	override fun onCreateOptionsMenu(menu: Menu?): Boolean {
 		menuInflater.inflate(R.menu.menu_items, menu)
 		return false
-	}
-
-	private fun addOnBackStackListener() {
-		supportFragmentManager.addOnBackStackChangedListener {
-			val currentFragment = supportFragmentManager.findFragmentById(R.id.root_fragment_container)
-			when (currentFragment) {
-				is FullScreenImageFragment -> {
-					supportActionBar?.apply {
-						title = null
-						hide()
-					}
-					window.decorView.apply {
-						systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE
-						systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-						systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-						systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-					}
-				}
-				is PictureDetailFragment -> {
-					supportActionBar?.apply {
-						title = getString(R.string.picture_of_the_day)
-						show()
-					}
-				}
-			}
-		}
 	}
 
 	private fun startPictureDetailFragment() {
@@ -62,9 +34,9 @@ class MainActivity : AppCompatActivity() {
 	fun startFullScreenImageFragment(url: String) {
 		supportFragmentManager
 			.beginTransaction()
-			.replace(R.id.root_fragment_container, FullScreenImageFragment.newInstance(url), FullScreenImageFragment.TAG)
+			.add(R.id.root_fragment_container, FullScreenImageFragment.newInstance(url), FullScreenImageFragment.TAG)
 			.addToBackStack(FullScreenImageFragment.TAG)
-			.commit()
+			.commitAllowingStateLoss()
 	}
 
 	fun showProgressBar() {
