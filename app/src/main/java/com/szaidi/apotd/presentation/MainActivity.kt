8b9335eb6f6aa8887db9b9cb77cec6a2da21c28a
Sajjad Zaidi <szaidi@tribalscale.com>
2018-08-04 15:33:29 -0400

    Adds options menu button to be able to refresh the view in case of errors.

--- app/src/main/java/com/szaidi/apotd/presentation/MainActivity.kt
+++ app/src/main/java/com/szaidi/apotd/presentation/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.szaidi.apotd.presentation
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import com.szaidi.apotd.R
 import com.szaidi.apotd.presentation.picturedetail.PictureDetailFragment
 
@@ -14,6 +16,20 @@ class MainActivity : AppCompatActivity() {
 		supportActionBar?.title = getString(R.string.picture_of_the_day)
 
 		startPictureDetailFragment()
+	}
+
+	override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+		menuInflater.inflate(R.menu.menu_items, menu)
+		return true
+	}
+
+	override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+		when (item?.itemId) {
+			R.id.action_refresh -> {
+				(supportFragmentManager.findFragmentById(R.id.root_fragment_container) as? PictureDetailFragment)?.fetchImage()
+			}
+		}
+		return super.onOptionsItemSelected(item)
 	}
 
 	private fun startPictureDetailFragment() {
