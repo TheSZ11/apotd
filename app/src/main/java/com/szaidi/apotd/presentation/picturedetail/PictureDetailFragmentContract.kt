656a773b7a886a4d20939dcf98422ac2f4aa59e2
Sajjad Zaidi <szaidi@tribalscale.com>
2018-08-04 13:52:28 -0400

    Implements MainActivity and PictureDetailFragment views, adds baseView interface for view contracts.

---
+++ app/src/main/java/com/szaidi/apotd/presentation/picturedetail/PictureDetailFragmentContract.kt
@@ -0,0 +1,13 @@
+package com.szaidi.apotd.presentation.picturedetail
+
+import com.szaidi.apotd.base.BaseView
+
+interface PictureDetailFragmentContract {
+	interface View : BaseView<Presenter> {
+
+	}
+
+	interface Presenter {
+
+	}
+}
