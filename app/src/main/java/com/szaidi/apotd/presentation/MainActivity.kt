0ec80759c87f34c5db017dced5810a426e111745
Sajjad Zaidi <szaidi@tribalscale.com>
2018-08-04 16:59:24 -0400

    Move loading spinner to activity, handle showing and hiding of spinners, actionbars, make fullScreenImageFragment start in immersive mode.

--- app/src/main/java/com/szaidi/apotd/presentation/MainActivity.kt
+++ app/src/main/java/com/szaidi/apotd/presentation/MainActivity.kt
@@ -3,9 +3,11 @@ package com.szaidi.apotd.presentation
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
-import android.view.MenuItem
+import android.view.View
 import com.szaidi.apotd.R
 import com.szaidi.apotd.presentation.picturedetail.PictureDetailFragment
+import com.szaidi.apotd.presentation.picturefullscreen.FullScreenImageFragment
+import kotlinx.android.synthetic.main.progress_view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,21 +17,39 @@ class MainActivity : AppCompatActivity() {
 
 		supportActionBar?.title = getString(R.string.picture_of_the_day)
 
+		addOnBackStackListener()
 		startPictureDetailFragment()
 	}
 
 	override fun onCreateOptionsMenu(menu: Menu?): Boolean {
 		menuInflater.inflate(R.menu.menu_items, menu)
-		return true
+		return false
 	}
 
-	override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-		when (item?.itemId) {
-			R.id.action_refresh -> {
-				(supportFragmentManager.findFragmentById(R.id.root_fragment_container) as? PictureDetailFragment)?.fetchImage()
+	private fun addOnBackStackListener() {
+		supportFragmentManager.addOnBackStackChangedListener {
+			val currentFragment = supportFragmentManager.findFragmentById(R.id.root_fragment_container)
+			when (currentFragment) {
+				is FullScreenImageFragment -> {
+					supportActionBar?.apply {
+						title = null
+						hide()
+					}
+					window.decorView.apply {
+						systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE
+						systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+						systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+						systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+					}
+				}
+				is PictureDetailFragment -> {
+					supportActionBar?.apply {
+						title = getString(R.string.picture_of_the_day)
+						show()
+					}
+				}
 			}
 		}
-		return super.onOptionsItemSelected(item)
 	}
 
 	private fun startPictureDetailFragment() {
@@ -37,5 +57,21 @@ class MainActivity : AppCompatActivity() {
 			.beginTransaction()
 			.replace(R.id.root_fragment_container, PictureDetailFragment.newInstance(), PictureDetailFragment.TAG)
 			.commit()
+	}
+
+	fun startFullScreenImageFragment(url: String) {
+		supportFragmentManager
+			.beginTransaction()
+			.replace(R.id.root_fragment_container, FullScreenImageFragment.newInstance(url), FullScreenImageFragment.TAG)
+			.addToBackStack(FullScreenImageFragment.TAG)
+			.commit()
+	}
+
+	fun showProgressBar() {
+		progress_view.visibility = View.VISIBLE
+	}
+
+	fun hideProgressBar() {
+		progress_view.visibility = View.GONE
 	}
 }
