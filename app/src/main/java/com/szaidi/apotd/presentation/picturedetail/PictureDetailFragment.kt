33de683b99ec0d281012db939de28eab9bbeeda2
Sajjad Zaidi <szaidi@tribalscale.com>
2018-08-04 16:58:42 -0400

    Create menu only in PictureDetailFragment, start fullScreenImageFragment on click of the image.

--- app/src/main/java/com/szaidi/apotd/presentation/picturedetail/PictureDetailFragment.kt
+++ app/src/main/java/com/szaidi/apotd/presentation/picturedetail/PictureDetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AlertDialog
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
@@ -11,11 +12,12 @@ import com.szaidi.apotd.R
 import com.szaidi.apotd.data.models.ApiErrorResponse
 import com.szaidi.apotd.data.models.PictureOfTheDay
 import com.szaidi.apotd.data.repositories.PictureRepository
+import com.szaidi.apotd.presentation.MainActivity
 import kotlinx.android.synthetic.main.picture_detail_fragment.*
-import kotlinx.android.synthetic.main.progress_view.*
 
 class PictureDetailFragment : Fragment(), PictureDetailFragmentContract.View {
 	private var presenter: PictureDetailFragmentContract.Presenter? = null
+	private var picture: PictureOfTheDay? = null
 
 	override fun setPresenter(presenter: PictureDetailFragmentContract.Presenter) {
 		this.presenter = presenter
@@ -29,16 +31,29 @@ class PictureDetailFragment : Fragment(), PictureDetailFragmentContract.View {
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 
+		setHasOptionsMenu(true)
+
 		fetchImage()
+		setOnClickListeners()
+	}
+
+	override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+		when (item?.itemId) {
+			R.id.action_refresh -> {
+				fetchImage()
+			}
+		}
+		return super.onOptionsItemSelected(item)
 	}
 
 	override fun onPictureFetched(picture: PictureOfTheDay) {
+		this.picture = picture
 		tv_title.text = picture.title
 		tv_exlanation.text = picture.explanation
 		Glide.with(context!!)
 			.load(picture.url)
 			.into(iv_image_of_the_day)
-		progress_view.visibility = View.GONE
+		hideProgressBar()
 	}
 
 	override fun onError(error: ApiErrorResponse) {
@@ -46,14 +61,30 @@ class PictureDetailFragment : Fragment(), PictureDetailFragmentContract.View {
 			setTitle(R.string.fetch_image_error)
 			setMessage(error.error.message)
 			setOnDismissListener {
-				progress_view.visibility = View.GONE
+				hideProgressBar()
 			}
 		}.create().show()
 	}
 
-	fun fetchImage(){
-		progress_view.visibility = View.VISIBLE
+	fun fetchImage() {
+		showProgressBar()
 		presenter?.fetchPicture()
+	}
+
+	private fun showProgressBar() {
+		(activity as? MainActivity)?.showProgressBar()
+	}
+
+	private fun hideProgressBar() {
+		(activity as? MainActivity)?.hideProgressBar()
+	}
+
+	private fun setOnClickListeners() {
+		iv_image_of_the_day.setOnClickListener {
+			picture?.let { picture: PictureOfTheDay ->
+				(activity as? MainActivity)?.startFullScreenImageFragment(picture.url)
+			}
+		}
 	}
 
 	companion object {
