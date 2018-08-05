61c425700ca47b084413d826f28de825b1c3e0f6
Sajjad Zaidi <szaidi@tribalscale.com>
2018-08-04 20:48:56 -0400

    Refactors code, changes api response to a Single.

--- app/src/main/java/com/szaidi/apotd/data/repositories/PictureRepository.kt
+++ app/src/main/java/com/szaidi/apotd/data/repositories/PictureRepository.kt
@@ -1,17 +1,16 @@
 package com.szaidi.apotd.data.repositories
 
 import com.szaidi.apotd.data.remote.ApiService
-import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import retrofit2.Response
 
 class PictureRepository(private val apiService: ApiService = ApiService.create()) {
 
-	fun get(date: String?): Observable<Response<Any>> {
+	fun get(date: String?): Single<Response<Any>> {
 		//todo: store locally in db? return from db when api errors?
 
 		return apiService.getImageOfTheDay(date)
 			.subscribeOn(Schedulers.io())
-			.toObservable()
 	}
 }
